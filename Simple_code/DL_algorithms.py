52395b68af62bbb426f3bca0ab81548915dae35a
PavanVenky <pavanvenkykaja@gmail.com>
2026-08-24 22:21:39 +0530

    Loss functions added

--- Simple_code/DL_algorithms.py
+++ Simple_code/DL_algorithms.py
@@ -1,7 +1,7 @@
 import torch
 
 def Categorical_cross_entropy():
-    softmax_out = torch.tensor([0.1,0.5,0.8,0.9,0.99])
+    softmax_out = torch.tensor([0.1,0.5,0.8,0.9,0.99]) # consider car as GT. model predicted outputs as softmax outputs. (probability 0.1-->very wrong..)
 
     #cross entropy
     CE = -torch.log(softmax_out)
@@ -12,6 +12,9 @@ def Categorical_cross_entropy():
     for idx,gamma in enumerate([1,2,3,4,5]):
         focal_loss = (1-softmax_out[idx])**gamma * CE[idx]      # Focal loss = CE * (1-pi)**gamma  here (1-pi)**gamma is focal factor
         print(focal_loss)
+
+    #Single line for focal loss: Focal Loss does NOT suppress hard/wrong examples very much. It mainly suppresses easy/correct examples.
+    # https://chatgpt.com/share/6a8c767c-9028-83e8-9fb1-34750b8103ac
 
 
 Categorical_cross_entropy()
